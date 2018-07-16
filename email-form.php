11067a28b4b72978fc3e378d489321cee6c00f6d
idelagarza82 <41305344+idelagarza82@users.noreply.github.com>
2018-07-16 17:11:24 -0500

    Update email-form.php

--- email-form.php
+++ email-form.php
@@ -3,8 +3,21 @@
   $visitor_email = $_Post['email'];
   $message = $_POST['message'];
   
-  $email_from = 'isaac.gerson@gmail.com';
+  $email_from = 'info@veocreativelab.com';
   
   $email_subject = "New Form Submission";
-                    "User Email": 
+
+  $email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Message: $message.\n";
+
+$to = "isaac.gerson@gmail.com";
+
+$headers = "From: email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html")
 ?>
