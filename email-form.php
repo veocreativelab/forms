2c9f3d8efd9d38050823a069f8147e3aacb83e7f
idelagarza82 <41305344+idelagarza82@users.noreply.github.com>
2018-07-16 16:58:13 -0500

    Create email-form.php

---
+++ email-form.php
@@ -0,0 +1,10 @@
+<?php
+  $name = $_Post['name'];
+  $visitor_email = $_Post['email'];
+  $message = $_POST['message'];
+  
+  $email_from = 'isaac.gerson@gmail.com';
+  
+  $email_subject = "New Form Submission";
+                    "User Email": 
+?>
